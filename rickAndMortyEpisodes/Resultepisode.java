8cfc70b1d220188d9db7a7907ca7ddda653bcfc5
Kyncev <45149572+Kyncev@users.noreply.github.com>
2023-08-28 20:18:29 +0300

    Add files via upload

---
+++ rickAndMortyEpisodes/Resultepisode.java
@@ -0,0 +1,19 @@
+package objects.rickAndMortyEpisodes;
+
+import objects.rickAndMortyCharacters.Location;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Resultepisode {
+    public int id;
+    public String name;
+    public String air_date;
+    public String episode;
+    public ArrayList<String> characters;
+    public String url;
+    public Location location;
+
+    public Date created;
+
+}
